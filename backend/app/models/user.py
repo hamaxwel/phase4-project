7c16f185fae56617d5d14bcda4caf170900a2b0b
maxwel kirimi <maxwelkirimii@gmail.com>
2025-01-31 17:06:15 +0300

    assignment

--- backend/app/models/user.py
+++ backend/app/models/user.py
@@ -5,7 +5,7 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     full_name = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(100), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
+    password = db.Column(db.String(512), nullable=False)
     phone_number = db.Column(db.String(20), nullable=False)
 
     def __repr__(self):
--- backend/config.py
+++ backend/config.py
@@ -2,7 +2,7 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = 'postgresql://bodadb_8s8r_user:N7T7Gx6Q4vUQNCy08w1d5crq4OwtSeUf@dpg-cuedb0dds78s73accesg-a.oregon-postgres.render.com/bodadb_8s8r'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     JWT_SECRET_KEY = 'your_jwt_secret_key'
     SECRET_KEY = 'your_secret_key'
